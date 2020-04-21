2261fe5b00fa0633bae67d3387f2fa3096d6071c
Marzia Fatarella <marziafatarella@gmail.com>
2020-04-21 15:57:25 +0200

    dadi

---
+++ Gioco dei dadi/main.js
@@ -0,0 +1,34 @@
+// "Gioco dei dadi" PC vs. Human: chiedi un numero all'utente tra 1 e 6 (giocata human) e estrai un numero casuale tra 1 e 6 (giocata PC). Chi fa il numero più alto vince!
+
+var tiro = prompt('Scrivi lancio per lanciare i dadi')
+console.log(tiro);
+// Giocatore 1
+// estrai un numero casuale tra 1 e 6 (pc)
+var giocatore1_dado1 = Math.floor(Math.random() * 6) + 1;
+
+var giocatore1_dado2 = Math.floor(Math.random() * 6) + 1;
+
+var risultato1 = giocatore1_dado1 + giocatore1_dado2;
+console.log('La somma dei tuoi dadi è ' + risultato1);
+document.getElementById('giocatore1').innerHTML='La somma dei tuoi dadi è ' + risultato1;
+// Giocatore 2
+// estrai un numero casuale tra 1 e 6 (pc)
+var giocatore2_dado1 = Math.floor(Math.random() * 6) + 1;
+
+var giocatore2_dado2 = Math.floor(Math.random() * 6) + 1;
+
+var risultato2 = giocatore2_dado1 + giocatore2_dado2;
+console.log('La somma dei dadi del tuo avversario è ' + risultato2);
+document.getElementById('giocatore2').innerHTML='La somma dei dadi del tuo avversario è ' + risultato2;
+// il più alto vince
+if (risultato1 > risultato2) {
+    console.log('Hai vinto!');
+    document.getElementById('haivinto').innerHTML='Hai vinto!';
+} else if(risultato1 == risultato2) {
+    console.log('Pareggio! Ritenta');
+    document.getElementById('pareggio').innerHTML='Pareggio! Ritenta';
+
+} else {
+    console.log('Hai perso!');
+    document.getElementById('haiperso').innerHTML='Hai perso!';
+}
