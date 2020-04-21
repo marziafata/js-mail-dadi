74a213a83aac5b6f4725613694cdc1fc244ed2bf
Marzia Fatarella <marziafatarella@gmail.com>
2020-04-21 16:56:42 +0200

    sei in lista

--- Sono in lista/main.js
+++ Sono in lista/main.js
@@ -2,9 +2,24 @@
 
 
 // creare un array con le email
+var archivio_indirizzi = ["marzia@boolean.it", "federica@boolean.it", "luca@boolean.it", "mirko@boolean.it", "valentina@boolean.it", "nicole@boolean.it", "fabio@boolean.it", "vittoria@boolean.it"];
+// console.log(archivio_indirizzi);
 
 // chiedere la mail all'utente
-
+var mail_utente = prompt('Inserisci la tua mail per sapere se sei già registrato:')
+// console.log('mail inserita da utente: ' + mail_utente);
 // controlla se la mail è dentro
+// (creare un ciclo for)
+for (var i = 0; i < archivio_indirizzi.length; i++) {
+    archivio_indirizzi[i]
+    console.log('archivio: ' + archivio_indirizzi);
+    // stampa un messaggio (sei in lista o no)
+    if (archivio_indirizzi[i] == mail_utente) {
+        // console.log('La tua mail è già presente nei nostri archivi. Non ricordi la password?');
+        document.getElementById('ok').innerHTML='La tua mail è già presente nei nostri archivi. Non ricordi la password?';
 
-// stampa un messaggio (sei in lista o no)
+    } else {
+        // console.log('La tua mail non è presente nei nostri archivi, vuoi registrarti ora?');
+        document.getElementById('no').innerHTML='La tua mail non è presente nei nostri archivi, vuoi registrarti ora?';
+    }
+}
