e43b985a3fda4124b9820370341cca0b07e4adb7
Marzia Fatarella <marziafatarella@gmail.com>
2020-04-21 16:03:21 +0200

    impostazione "sei in lista"

---
+++ Sono in lista/main.js
@@ -0,0 +1,10 @@
+// "Sono in lista?" Chiedi all'utente la sua email e controlla che sia nella lista di chi può accedere (ergo: la mail è presente in un array contenente le email "autorizzate") e stampa un messaggio appropriato (sei in lista/non sei in lista)
+
+
+// creare un array con le email
+
+// chiedere la mail all'utente
+
+// controlla se la mail è dentro
+
+// stampa un messaggio (sei in lista o no)
